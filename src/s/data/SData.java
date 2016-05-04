3ba6d1f2e3a7e42fb5f5823e5518ab76cafecfc6
Nightfury <Nightfury@Farzad>
2016-05-04 22:34:52 +0430

    test 2 commit

--- src/s/data/SData.java
+++ src/s/data/SData.java
@@ -15,6 +15,7 @@ public class SData {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("");
         // TODO code application logic here
     }
     
